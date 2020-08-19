48e09d9c0dde7eabefe5a358c16cd89f82363c7c
kelvismnd <kelvismendes@gmail.com>
2020-08-19 19:45:22 -0300

    adicionando marko, para gerar html

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -4,7 +4,9 @@ module.exports = (app)=>{
         resp.send("<h1>pagina home</h1>");
     }); 
     app.get('/livros',(req,resp)=>{
-        resp.send("<h1>pagina Livros</h1>");
+        resp.marko(
+            require('../views/livros/lista/lista.marko')
+        );
     }); 
     
 }   
